620c6ee5f6e19dddf69d189eff4ba6e386ae0c5c
Derek Mason <3derekmason@gmail.com>
2022-07-29 09:01:16 -0600

    map multiple greetings

---
+++ greetings/go.mod
@@ -0,0 +1,3 @@
+module github.com/going/hello
+
+go 1.18
--- hello/hello.go
+++ hello/hello.go
@@ -3,7 +3,6 @@ package main
 import (
     "fmt"
     "log"
-
     "github.com/3derekmason/going/greetings"
 )
 
@@ -14,15 +13,15 @@ func main() {
     log.SetPrefix("greetings: ")
     log.SetFlags(0)
 
-    // Request a greeting message.
-    message, err := greetings.Hello("Gladys")
-    // If an error was returned, print it to the console and
-    // exit the program.
-    if err != nil {
-        log.Fatal(err)
-    }
+  // A slice of names.
+  names := []string{"Derek", "Beej", "Abby", "Oliver"}
 
-    // If no error was returned, print the returned message
-    // to the console.
-    fmt.Println(message)
+  // Request greeting messages for the names.
+  messages, err := greetings.Hellos(names)
+  if err != nil {
+      log.Fatal(err)
+  }
+  // If no error was returned, print the returned map of
+  // messages to the console.
+  fmt.Println(messages)
 }
